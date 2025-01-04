3764d669bbe98d0dcc86db7025e5c52deec2d77c
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-01-04 14:42:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || false,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "Nikka-XJ2gB0L5B#3EIckMQg73EIjh7Xb_sO24PUPDtwZyucWzFqSpt4S9M",
+  SESSION_ID:process.env.SESSION_ID || "Nikka-XkmQmFSiY#VQPqSDARo43zOSeG6b2RAFzHzAJ9VPsL_qTrBckR_Ac",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[!]',
   PRESCENCE: process.env.PRESCENCE || "typing",
